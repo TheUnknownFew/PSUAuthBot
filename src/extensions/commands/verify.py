c34a9bdb098cef015d31ab49fdcf262338391ed6
Vase of Petunias <TheUnknownFew@users.noreply.github.com>
2021-11-10 13:29:34 -0500

    prototype finalization. testing and deployment imminent.

--- src/extensions/commands/verify.py
+++ src/extensions/commands/verify.py
@@ -5,7 +5,6 @@ from discord import Message, DMChannel
 from discord.ext import commands, tasks
 
 from common.bot import userstatus, views
-from common.bot.verification import VerificationManager
 from common.data.userdb import UserEntryManager
 from common.exceptions import InvalidEmail, ConfirmationEmailMismatch, UserUpdateError
 from common.bot.email import Gmail
@@ -50,18 +49,21 @@ class Verify(commands.Cog):
         to the user verification status message for admins to manually review.
         :param message: The message sent passed in from the event on_message.
         """
-        if self.__bot.user.id != message.author.id and message.guild is None:
-            verification_manager = await VerificationManager.from_discord_user(message.author)
-            if not verification_manager.is_user_registered:
-                return
-
-            img_urls = [att.proxy_url for att in message.attachments if att.content_type.startswith('image')] + \
-                       [embed.url for embed in message.embeds if embed.type in ['image', 'gifv', 'link']]
+        if self.__bot.user.id != message.author.id:
             ctx: commands.Context = await self.__bot.get_context(message)
-            if len(img_urls) == 0:
-                await ctx.send(embed=emb.IMAGE_NOT_FOUND)
-                return
-            if verification_manager.accept_canvas_images(img_urls):
+            has_status = await userdb.is_user_registered_with_status(message.author, userstatus.is_user_pending_dm)
+            if message.guild is None and has_status:
+                img_urls = [att.proxy_url for att in message.attachments if att.content_type.startswith('image')] + \
+                           [embed.url for embed in message.embeds if embed.type in ['image', 'gifv', 'link']]
+                if len(img_urls) == 0:
+                    await ctx.send(embed=emb.IMAGE_NOT_FOUND)
+                    return
+                async with UserEntryManager(ctx.author) as user:
+                    user_entry = await user.get_user_entry()
+                    user_entry.next_status(userstatus.canvas_image_received)
+                    user_entry.image_urls = img_urls
+                    await user.update_user(user_entry)
+                await views.loaded_status_views[user_entry.user_id].update_status_message(new_data=user_entry)
                 await ctx.send(embed=emb.FINAL_DM)
 
     @tasks.loop(seconds=dcfg.email_refresh_rate)
@@ -112,52 +114,10 @@ class Verify(commands.Cog):
                 await user.register_user(user_entry)
                 self.__gmail.send_email_to(email)
             else:
-                # Todo: 'update' user information embed - tell user to use update
-                pass
+                await ctx.send('Looks like you already executed this command. If you are experiencing an issue, please contact a greeter.', delete_after=emb.ERR_DELAY)
         await ctx.message.delete(delay=emb.SUCCESS_DELAY)
 
-    @verify.command(
-        name='email',
-        aliases=['e'],
-        usage='!update email <psu email> <confirm email>',
-        brief='!update email jas1234@psu.edu jas1234@psu.edu')
-    async def update_email(self, ctx: commands.Context, email: str, confirm_email: str):
-        async with UserEntryManager(ctx.author) as user:
-            if not user.__is_registered:
-                raise UserUpdateError(ctx.author)
-            if not match('[a-zA-Z]+[0-9]+@psu.edu', email):  # check if valid Pennstate email
-                raise InvalidEmail(ctx.author.name, email)
-            if email != confirm_email:  # check if confirmation email matches
-                raise ConfirmationEmailMismatch(ctx.author.name, email, confirm_email)
-
-            user_entry = await user.get_user_entry()
-            if userstatus.is_user_pending_email(user_entry.status):
-                self.__gmail.delete_email(email)
-                await self.resend(ctx)
-
-    @verify.command(
-        name='name',
-        aliases=['n'],
-        usage='!update name <first name> <last name>',
-        brief='!update name John Smith')
-    async def update_name(self, ctx: commands.Context, first_name: str, last_name: str):
-        async with UserEntryManager(ctx.author) as user:
-            if not user.__is_registered:
-                raise UserUpdateError(ctx.author)
-
-            user_entry = await user.get_user_entry()
-            if user_entry.status <= UserStatus.AWAITING_VERIFICATION:
-                user_entry.first_name = first_name
-                user_entry.last_name = last_name
-                await views.loaded_status_views[user_entry.user_id].update_status_message(new_data=user_entry)
-
-    @commands.command()
-    async def resend(self, ctx: commands.Context):
-        pass
-
     @verify.error
-    @update_email.error
-    @update_name.error
     async def handle_error(self, ctx: commands.Context, err):
         await ctx.message.delete(delay=emb.ERR_DELAY)
         try:
